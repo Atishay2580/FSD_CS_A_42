81c5bf2820eb4bb8d5e373804a546871ba2d53d7
Atishay Jain <atishay2580@gmail.com>
2024-11-11 10:29:31 +0530

    Start React

---
+++ My-React-App/react-cdn.js
@@ -0,0 +1,13 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom'
+
+const parent = document.getElementById("root");
+const element = React.createElement("h1", {}, "Shopping Cart App");
+const root = ReactDOM.createRoot(parent);
+
+const item1 = React.createElement("li",{},"Item 1")
+const item2 = React.createElement("li",{},"Item 2")
+const item3 = React.createElement("li",{},"Item 3")
+const list = React.createElement("ul",{},item1,item2,item3)
+
+root.render([element,list]);
